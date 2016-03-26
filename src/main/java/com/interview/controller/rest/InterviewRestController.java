820b42a1f424b5fc9800e41eea36e75066e64949
Artem Baranovskiy <artem2549@gmail.com>
2016-03-26 17:44:42 +0200

    * crud rest for interview * working tests for rest controller

--- src/main/java/com/interview/controller/rest/InterviewRestController.java
+++ src/main/java/com/interview/controller/rest/InterviewRestController.java
@@ -1,9 +1,115 @@
 package com.interview.controller.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.interview.model.Interview;
+import com.interview.model.InterviewQuestion;
+import com.interview.service.InterviewService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+/**
+ * Simple rest controller which is responsible for C.R.U.D.
+ * and for interview manipulating operations:
+ *      GET    - /rest/interviews     - get all interviews
+ *      GET    - /rest/interviews/id  - get interview with specified id
+ *      POST   - /rest/interviews     - add interview
+ *      PUT    - /rest/interviews/id  - update interview with specified id
+ *      DELETE - /rest/interviews/id  - delete interview from the repository
+ *
+ * Simple way to create interview with specified interviewer is to set interviewer id
+ * in interviewer field
+ *
+ * @author Artem Baranovskiy
+ */
 @RestController
 @RequestMapping("/rest/interviews")
 public class InterviewRestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InterviewRestController.class);
+
+    @Autowired
+    private InterviewService interviewService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity readAllInterviews() {
+        final List<Interview> interviews = interviewService.readAllInterviews();
+        if (interviews == null) {
+            return new ResponseEntity(NO_CONTENT);
+        } else {
+            LOG.info("Fetching all interviews: OK");
+            return new ResponseEntity<>(interviews, OK);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity readInterview(@PathVariable("id") String id) {
+        final Interview interview = interviewService.readInterview(id);
+        if (interview == null) {
+            LOG.error("Fetching {} interview: NO_CONTENT", id);
+            return new ResponseEntity<>(NO_CONTENT);
+        } else {
+            LOG.info("Fetching {} interview: OK", id);
+            return new ResponseEntity<>(interview, OK);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity createInterview(@RequestBody @Valid Interview interview, BindingResult result,
+                                          UriComponentsBuilder uriComponentsBuilder) {
+        if (result.hasErrors()) {
+            LOG.error("Creating interview: {}, BAD_REQUEST", result.getFieldError().getDefaultMessage());
+            return new ResponseEntity<>(result.getFieldError().getDefaultMessage(), BAD_REQUEST);
+        }
+        if ((interview = interviewService.createInterview(interview)) != null) {
+            LOG.info("Creating interview: CREATED");
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(uriComponentsBuilder.path("/rest/interviews/{id}").
+                    buildAndExpand(interview.getId()).toUri());
+            return new ResponseEntity<>(interview, headers, CREATED);
+        } else {
+            LOG.error("Creating interview: CONFLICT");
+            return new ResponseEntity(CONFLICT);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity updateInterview(@RequestBody @Valid Interview interview,
+                                 BindingResult result,
+                                 @PathVariable("id") String id) {
+        if (result.hasErrors()) {
+            LOG.error("Updating interview: {}, BAD_REQUEST", result.getFieldError().getDefaultMessage());
+            return new ResponseEntity<>(result.getFieldError().getDefaultMessage(), BAD_REQUEST);
+        }
+        interview.setId(id);
+        if (interviewService.readInterview(id) != null
+                && interviewService.updateInterview(interview) != null ) {
+            LOG.info("Updating interview: OK");
+            return new ResponseEntity(OK);
+        } else {
+            LOG.error("Updating interview: NO_CONTENT");
+            return new ResponseEntity(NO_CONTENT);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteInterview(@PathVariable("id") String id) {
+        if (interviewService.deleteInterview(id)) {
+            LOG.info("Deleting interview: OK");
+            return new ResponseEntity(OK);
+        } else {
+            LOG.error("Deleting interview: NO_CONTENT");
+            return new ResponseEntity(NO_CONTENT);
+        }
+    }
+
 }
--- src/test/java/com/interview/controller/rest/InterviewRestControllerTest.java
+++ src/test/java/com/interview/controller/rest/InterviewRestControllerTest.java
@@ -1,5 +1,156 @@
 package com.interview.controller.rest;
 
-public class InterviewRestControllerTest {
+import com.interview.config.MvcConfigurer;
+import com.interview.model.Interview;
+import com.jayway.restassured.RestAssured;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.http.ContentType.JSON;
+import static org.apache.http.HttpStatus.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * @author Artem Baranovskiy
+ */
+@SpringApplicationConfiguration(MvcConfigurer.class)
+@WebIntegrationTest("server.port:0")
+@DirtiesContext
+public class InterviewRestControllerTest extends AbstractTestNGSpringContextTests {
+
+    @Value("${local.server.port}")
+    protected int port;
+
+    private String id;
+
+    private Interview interview;
+
+    @BeforeClass
+    public void initInterview() {
+        interview = new Interview();
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void createdWhenSuccessInterviewCreating() {
+        id = given()
+                .contentType(JSON)
+                .body(interview)
+        .when()
+                .post("/rest/interviews")
+        .then()
+                .statusCode(SC_CREATED)
+                .extract()
+                .jsonPath()
+                .getString("id").replaceAll("\\[",  "").replaceAll("\\]", "");
+    }
+
+    @Test(dependsOnMethods = "createdWhenSuccessInterviewCreating")
+    public void badRequestWhenCreateInterviewWithNoBody() {
+        given()
+                .contentType(JSON)
+        .when()
+                .post("/rest/interviews")
+        .then()
+                .statusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(dependsOnMethods = "badRequestWhenCreateInterviewWithNoBody")
+    public void okWhenReadInterview() {
+        given()
+                .contentType(JSON)
+        .when()
+                .get("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_OK)
+                .body("id", notNullValue())
+                .body("interviewer", nullValue())
+                .body("questions", nullValue())
+                .body("maxValue", is(0f))
+                .body("finalValue", is(0f))
+                .body("questions", nullValue());
+    }
+
+    @Test(dependsOnMethods = "okWhenReadInterview")
+    public void okWhenReadAllInterviews() {
+        given()
+                .contentType(JSON)
+        .when()
+                .get("/rest/interviews")
+        .then()
+                .statusCode(SC_OK);
+    }
+
+    @Test(dependsOnMethods = "okWhenReadAllInterviews")
+    public void okWhenUpdateExistedInterview() {
+        interview.addComment("CommentFromTest");
+        given()
+                .contentType(JSON)
+                .body(interview)
+        .when()
+                .put("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_OK)
+                .extract()
+                .jsonPath()
+                .param("comments", hasItem("CommentFromTest"));
+    }
+
+    @Test(dependsOnMethods = "okWhenUpdateExistedInterview")
+    public void badRequestWhenUpdateInterviewWithNoBody() {
+        given()
+                .contentType(JSON)
+        .when()
+                .put("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_BAD_REQUEST);
+    }
+
+    @Test(dependsOnMethods = "badRequestWhenUpdateInterviewWithNoBody")
+    public void noContentWhenUpdateNonexistentInterview() {
+        given()
+                .contentType(JSON)
+                .body(interview)
+        .when()
+                .put("/rest/interviews/001100")
+        .then()
+                .statusCode(SC_NO_CONTENT);
+    }
+
+    @Test(dependsOnMethods = "noContentWhenUpdateNonexistentInterview")
+    public void okWhenDeleteExistedInterview() {
+        given()
+                .contentType(JSON)
+        .when()
+                .delete("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_OK);
+    }
+
+    @Test(dependsOnMethods = "okWhenDeleteExistedInterview")
+    public void noContentWhenReadNonexistentInterview() {
+        given()
+                .contentType(JSON)
+        .when()
+                .get("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_NO_CONTENT);
+    }
+
+    @Test(dependsOnMethods = "noContentWhenReadNonexistentInterview")
+    public void noContentWhenDeleteNonExistentInterview() {
+        given()
+                .contentType(JSON)
+        .when()
+                .delete("/rest/interviews/" + id)
+        .then()
+                .statusCode(SC_NO_CONTENT);
+    }
 
 }
