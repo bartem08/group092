90b63d9ff5eef79052fe3b415ff95d363df51b64
aniikabelu <aniikabelu@gmail.com>
2016-03-31 23:16:57 +0300

    * added annotation DBRef in class Candidate * added annotation service in class CandidateServiceImpl * added rest controllers to class CandidateRestController

--- src/main/java/com/interview/service/mongo/CandidateServiceImpl.java
+++ src/main/java/com/interview/service/mongo/CandidateServiceImpl.java
@@ -6,9 +6,11 @@ import com.interview.repository.CandidateRepository;
 import com.interview.repository.InterviewRepository;
 import com.interview.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CandidateServiceImpl implements CandidateService{
     @Autowired
     private CandidateRepository candidateRepository;
